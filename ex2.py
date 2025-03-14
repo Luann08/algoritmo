c142f07431bd500255da4305f0bec7e68c08ee4f
Luann08 <luann082013@gmail.com>
2025-03-13 21:54:00 -0300

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#Problema: Escreva um algoritmo para ler as dimensões de um retangulo (base e altura), calcular e escrever a área do retangulo 
+
+base = int(input("Me fale a base do retangulo:\n"))
+altura = int(input("me fale a altura do retangulo:\n"))
+
+print(f"A área do retangulo é {base * altura}")
