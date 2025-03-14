1e49e52ebd0445a4b91590d531d6c011479fba9b
Luann08 <luann082013@gmail.com>
2025-03-13 21:40:58 -0300

    Create ex1.py

---
+++ algoritmo.py
@@ -0,0 +1,4 @@
+#Problema: Escreva um algoritmo para ler um valor numérico e escrever na tela o seu antecessor
+
+valor = int(input("Me fale um número:\n"))
+print(f"O antecessor do {valor} é {valor - 1}")
